b43413848d561ddba77abe3d4d66a001fc417f39
deborabastos <deborabastos14@gmail.com>
2020-01-21 19:24:18 -0300

    Ainda alterando footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,63 +1,50 @@
-<footer class="bg-primary">
-    <div class="container-fluid">
-    <div class="row">
-        <div class="col-sm">
-        <div class="footer-items">    
-            <h2 class="text-white text-uppercase">localizaCão</h1>
-            <p class="text-muted">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam, ab.</p>
-        </div>
+<footer class="footer">
+         <div class="inner-footer">
+            <div class="footer-items">
+                <h1 class="footer-h1">LocalizaCão</h1>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam, ab.</p>
+            </div>
 
-        </div>
-        <div class="col-sm">
-        <div class="footer-items">
-                    <h5 class="text-uppercase">Adotar</h4>
-                    <div class="footer-items-border"></div>
+            <div class="footer-items">
+                    <h2 class="footer-h2">ADOTAR</h2>
+                    <div class="footer-border"></div>
                     <ul>
-                        <a class="text-white" href="">Adocao</a>
-                        <a class="text-white" href="">Procurar</a>
-                        <a class="text-white" href="">Casa para bichinho</a>
-                        
-
+                        <li><a href="">Adocao</a></li>
+                        <li><a href="">Procurar</a></li>
+                        <li><a href="">Casa para bichinho</a></li>  
                     </ul>
             </div>
-        </div>
-        <div class="col-sm">
-        <div class="footer-items">
-                    <h5 class="text-uppercase">another</h4>
-                    <div class="footer-items-border"></div>
-                    <ul>
-                        <a class="text-white" href="">home</a>
-                        <a class="text-white" href="">home</a>
-                        <a class="text-white" href="">home</a>
-                        <a class="text-white" href="">home</a>
-                        <a class="text-white" href="">home</a>
 
+            <div class="footer-items">
+                    <h2 class="footer-h2">ANOTHER</h2>
+                    <div class="footer-border"></div>
+                    <ul>
+                        <li><a href="">home</a></li>
+                        <li><a href="">home</a></li>
+                        <li><a href="">home</a></li>
+                        <li><a href="">home</a></li>
                     </ul>
             </div>
-        </div>
-        <div class="col-sm">
-        <div class="footer-items">
-                    <h5 class="text-uppercase">contato</h4>
-                    <div class="footer-items-border"></div>
-                    <ul>
-                     <li><i class="fa fa-map-marker" aria-hidden="true"></i> 4455 av interlagos</li>
-                     <li><i class="fa fa-phone" aria-hidden="true"></i> 11- 997979981</li>
-                     <li><i class="fa fa-envelope" aria-hidden="true"></i> support@gmail.com</li>
 
+            <div class="footer-items">
+                    <h2 class="footer-h2">CONTATO</h2>
+                    <div class="footer-border"></div>
+                    <ul>
+                        <li><i class="fa fa-map-marker" aria-hidden="true"></i> 4455 av interlagos</li>
+                        <li><i class="fa fa-phone" aria-hidden="true"></i> 11- 997979981</li>
+                        <li><i class="fa fa-envelope" aria-hidden="true"></i> support@gmail.com</li>
                     </ul>
+
                     <div class="social-media">
                         <a href=""><i class="fa fa-facebook" aria-hidden="true"></i></a>
                         <a href=""><i class="fa fa-twitter" aria-hidden="true"></i></a>
-                        <a href=""><i class="fa fa-instagram"  aria-hidden="true"></i></a>
+                        <a href=""><i class="fa fa-instagram" aria-hidden="true"></i></a>
                         <a href=""><i class="fa fa-google-plus" aria-hidden="true"></i></a>
-
                     </div>
             </div>
-        </div>
-    </div>
 
-
-
-
-</div>
+         </div>
+         <div class="footer-bottom">
+             copyright &copy; localizadog 2020. all rights reserved.
+         </div>
 </footer>
