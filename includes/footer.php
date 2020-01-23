fa5f906996cca7ae6223d458e038e85fb52efaed
deborabastos <deborabastos14@gmail.com>
2020-01-22 21:07:23 -0300

    Arrumando footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,50 +1,48 @@
 <footer class="footer">
-         <div class="inner-footer">
-            <div class="footer-items">
-                <h1 class="footer-h1">LocalizaCão</h1>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam, ab.</p>
-            </div>
+     <div class="inner-footer">
+        <div class="footer-items">
+            <h1 class="footer-h1"><a class="footer-h1" href="index.php">LocalizaCão</a></h1>
+            <p>O LocalizaCão é um serviço gratuito, criado para te ajudar a encontrar seu amiguinho.</p>
+        </div>
 
-            <div class="footer-items">
-                    <h2 class="footer-h2">ADOTAR</h2>
-                    <div class="footer-border"></div>
-                    <ul>
-                        <li><a href="">Adocao</a></li>
-                        <li><a href="">Procurar</a></li>
-                        <li><a href="">Casa para bichinho</a></li>  
-                    </ul>
-            </div>
+        <div class="footer-items">
+            <h2 class="footer-h2">QUEM SOMOS?</h2>
+            <div class="footer-border"></div>
+            <ul>
+                <li><a href="quemsomos.php">Sobre nós</a></li>
+                <li><a href="sucesso.php">Casos de sucesso</li></a>                                                
+                <li><i class="fa fa-phone" aria-hidden="true"></i> 11- 997979981</li>
+                <li><i class="fa fa-envelope" aria-hidden="true"></i> support@gmail.com</li>
+            </ul>
+        </div>
 
-            <div class="footer-items">
-                    <h2 class="footer-h2">ANOTHER</h2>
-                    <div class="footer-border"></div>
-                    <ul>
-                        <li><a href="">home</a></li>
-                        <li><a href="">home</a></li>
-                        <li><a href="">home</a></li>
-                        <li><a href="">home</a></li>
-                    </ul>
-            </div>
 
-            <div class="footer-items">
-                    <h2 class="footer-h2">CONTATO</h2>
-                    <div class="footer-border"></div>
-                    <ul>
-                        <li><i class="fa fa-map-marker" aria-hidden="true"></i> 4455 av interlagos</li>
-                        <li><i class="fa fa-phone" aria-hidden="true"></i> 11- 997979981</li>
-                        <li><i class="fa fa-envelope" aria-hidden="true"></i> support@gmail.com</li>
-                    </ul>
+        <div class="footer-items">
+            <h2 class="footer-h2">MAPA DO SITE</h2>
+            <div class="footer-border"></div>
+            <ul>
+                <li><a href="achados.php">Achados</a></li>
+                <li><a href="perdidos.php">Perdidos</a></li>
+                <li><a href="adote.php">Adote</a> </li>
+            </ul>
+        </div>
 
-                    <div class="social-media">
-                        <a href=""><i class="fa fa-facebook" aria-hidden="true"></i></a>
-                        <a href=""><i class="fa fa-twitter" aria-hidden="true"></i></a>
-                        <a href=""><i class="fa fa-instagram" aria-hidden="true"></i></a>
-                        <a href=""><i class="fa fa-google-plus" aria-hidden="true"></i></a>
-                    </div>
-            </div>
+        <div class="footer-items">
+            <h2 class="footer-h2">FIQUE POR DENTRO!</h2>
+            <div class="footer-border"></div>
+            <p>Para receber notícias e novidades do nosso site, deixe seu e-mail: </p>
+            <ul>
+                <li><a href="">  
+                    <input type="email" name="" id="" placeholder="Informe seu e-mail">
+                    <button class="button-cadastro" type="submit" href="#">Enviar</button>
+                </a></li>                  
+                        
+                <li><a class="privacidade" href="#">Política de Privacidade</a></li> <!-- Abrir popup com política de privacidade -->
+            </ul>
+        </div>
 
-         </div>
-         <div class="footer-bottom">
-             copyright &copy; localizadog 2020. all rights reserved.
-         </div>
+
+    <div class="footer-bottom">
+        copyright &copy; LocalizaCão 2020. all rights reserved.
+    </div>
 </footer>
