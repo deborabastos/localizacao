c7866953b0245e701256b62ff224e84d4ef46822
deborabastos <deborabastos14@gmail.com>
2020-01-22 21:09:17 -0300

    Arrumando header

--- includes/nav.php
+++ includes/nav.php
@@ -1,45 +1,60 @@
-<nav class="navbar navbar-fixed-top navbar-expand-lg navbar-dark bg-primary">
+<!-- Topo -->
+<div class="topo">
+  <section class="top-menu">
+    <ul class="social-menu-top">
+      <li><a href="http://www.twitter.com/" target="_blank"><i class="fa fa-twitter"></i></a></li>
+      <li><a href="https://instagram.com/" target="_blank"><i class="fa fa-instagram"></i></a></li>
+      <li><a href="https://www.facebook.com/" target="_blank"><i class="fa fa-facebook"></i></a></li>
+      <li><a href="https://www.youtube.com/" target="_blank"><i class="fa fa-youtube"></i></a></li>
+      <li><a href="http://www.pinterest.com/" target="_blank"><i class="fa fa-pinterest"></i></a></li>                        
+    </ul>
+  </section>
+</div>
+
+<!-- Navbar -->
+<div class="minha_nav">
+  <nav class="navbar navbar-fixed-top navbar-expand-lg navbar-light bg-light">   
+    
+    <!-- Logo -->
+    <div class="logo">
+      <a class="logo_link" href="index.php">
+        <img src="images/logo_sn.jpg" alt="Localizacão">
+        <p class="logo_p">LocalizaCão</p>
+      </a>
+    </div>
+  
+    <!-- Menu Hamburguer -->
+    <button class="navbar-toggler ml" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Menu de navegação">
+      <span class="navbar-toggler-icon ml"></span>
+    </button>
+
+    <!-- Menu Normal -->  
+    <div class="collapse navbar-collapse mt-2" id="navbarNavDropdown">
+      <!-- Links da página -->
+      <ul class="navbar-nav mx-auto">
+        <li class="nav-item">
+          <a class="nav-link h5 text-uppercase text-primary mx-2" href="achados.php">Achados</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link h5 text-uppercase text-primary mx-2" href="perdidos.php">Perdidos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link h5 text-uppercase text-primary mx-2" href="adote.php">Adote</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link h5 text-uppercase text-primary mx-2" href="quemsomos.php">Quem somos</a>
+        </li>
+      </ul>
       
-      <div class="logo">
-        <img src="images/logo2.jpg" alt="Localizacão">
-      </div>
-      
-
-      
-      <!-- Menu Hamburguer -->
-      <button class="navbar-toggler ml" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Menu de navegação">
-        <span class="navbar-toggler-icon ml"></span>
-      </button>
-      <!-- Menu hamburguer posicionado estranho com uso da siv container!!!!!!!!!!! -->
-      <!-- Configurar melhor letra do menu hamburguer !!!!!!-->
-
-      <!-- Menu Normal -->  
-      <div class="container">
-      <div class="collapse navbar-collapse mt-2" id="navbarNavDropdown">
-        <ul class="navbar-nav mx-auto">
-          <li class="nav-item">
-            <a class="nav-link h5 text-uppercase text-secondary mx-2" href="index.php">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link h5 text-uppercase text-secondary mx-2" href="achados.php">Achados</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link h5 text-uppercase text-secondary mx-2" href="perdidos.php">Perdidos</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link h5 text-uppercase text-secondary mx-2  " href="adote.php">Adote</a>
-          </li>
-        </ul>
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item">
-            <a class="button btn-secondary font-weight-bold text-primary rounded p-2 mx-2" href="#" role="button">Login</a>
-          </li>
-          <li class="nav-item">
-          <a class="button btn-secondary font-weight-bold text-primary rounded p-2" href="#" role="button">Cadastro</a>          </li>
-        </ul>
-        
-
-
+      <!-- Botões -->
+      <ul class="nav-button navbar-nav ml-auto">
+        <li class="nav-item mt-2 mr-2 mb-2">
+          <a class="button btn-primary font-weight-bold rounded p-2" href="#" role="button">Login</a>
+        </li>
+        <li class="nav-item mt-2 mb-2">
+          <a class="button btn-primary font-weight-bold rounded p-2" href="#" role="button">Cadastro</a>          
+        </li>
+      </ul>
     </div>
   </nav>
 </div>
