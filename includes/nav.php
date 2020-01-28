e8239497f3a0eda167f5968d21572716fecd2171
RomildoOliveira <romildo_lira@yahoo.com.br>
2020-01-28 12:59:07 -0300

    Alterada a página de cadastro de usuário para formulário em pop up

--- includes/nav.php
+++ includes/nav.php
@@ -80,10 +80,130 @@
           </div>
           </li>
 
-        <li class="nav-item mt-1 ">
-          <a class="btn-primary btn-sm font-weight-bold rounded p-2 " href="cadastro_usuario.php" role="button">Cadastro</a>          
-        </li>
-      </ul>
+
+          <!--formulário de cadastro Suspenso-->
+          <button type="button" class="btn btn-primary btn-sm font-weight-bold mr-2 mb-3 p-1" data-toggle="modal" data-target="#exampleModal">
+          Cadastrar
+          </button>
+
+
+          <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+          <div class="modal-content">
+          <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Cadastro</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+          </button>
+          </div>
+          <div class="modal-body">
+
+          <form action="#" class="form-horizontal">
+					<fieldset>
+						<legend>Dados Pessoais</legend>
+						<div class="form-group">
+							<label for="nome" class="col-sm-2 control-label">Nome</label>
+							<div class="col-sm-4">
+								<input type="text" class="form-control" id="nome">
+							</div>
+						</div>
+						
+						<div class="form-group">
+							<label class="col-sm-2 control-label">Sexo</label>
+							<div class="col-sm-4">
+								<label class="radio-inline">
+									<input type="radio" name="sexo" id="feminino" value="FEMININO"> Feminino
+								</label>
+
+								<label class="radio-inline">
+									<input type="radio" name="sexo" id="masculino" value="MASCULINO"> Masculino
+								</label>
+							</div>
+						</div>
+						
+						<div class="form-group">
+							<label for="data-nascimento" class="col-sm-2 control-label">Data de Nascimento</label>
+							<div class="col-sm-4">
+								<input type="text" class="form-control" id="data-nascimento">
+							</div>
+						</div>
+					</fieldset>
+					
+					<fieldset>
+						<legend>Contato</legend>
+						<div class="form-group">
+							<label for="email" class="col-sm-2 control-label">Email</label>
+							<div class="col-sm-4">
+								<input type="email" class="form-control" id="email">
+							</div>
+						</div>
+						
+						<div class="form-group">
+							<label for="telefone" class="col-sm-2 control-label">Telefone</label>
+							<div class="col-sm-4">
+								<input type="tel" class="form-control" id="telefone">
+							</div>
+						</div>
+					</fieldset>
+					
+					<fieldset>
+						<legend>Endereço</legend>
+						<div class="form-group">
+							<label for="logradouro" class="col-sm-2 control-label">Logradouro</label>
+							<div class="col-sm-4">
+								<input type="text" class="form-control" id="logradouro">
+							</div>
+						</div>
+						
+						<div class="form-group">
+							<label for="uf" class="col-sm-2 control-label">UF</label>
+							<div class="col-sm-4">
+								<select class="form-control" name="uf" id="uf">
+									<option value="AC">AC</option>
+									<option value="AL">AL</option>
+									<option value="AM">AM</option>
+									<option value="AP">AP</option>
+									<option value="BA">BA</option>
+									<option value="CE">CE</option>
+									<option value="DF">DF</option>
+									<option value="ES">ES</option>
+									<option value="GO">GO</option>
+									<option value="MA">MA</option>
+									<option value="MG">MG</option>
+									<option value="MS">MS</option>
+									<option value="MT">MT</option>
+									<option value="PA">PA</option>
+									<option value="PE">PE</option>
+									<option value="PI">PI</option>
+									<option value="PR">PR</option>
+									<option value="RJ">RJ</option>
+									<option value="RN">RN</option>
+									<option value="RO">RO</option>
+									<option value="RR">RR</option>
+									<option value="RS">RS</option>
+									<option value="SC">SC</option>
+									<option value="SE">SE</option>
+									<option value="SP">SP</option>
+									<option value="TO">TO</option>
+								</select>
+							</div>
+						</div>
+					</fieldset>
+					
+					<button type="submit" class="btn btn-primary">
+						<span class="glyphicon glyphicon-floppy-disk" aria-hidden="true"></span> Enviar
+					</button>
+				</form>
+
+
+          </div>
+          
+          </div>
+          </div>
+          </div>
+         
+
+
     </div>
   </nav>
 </div>
