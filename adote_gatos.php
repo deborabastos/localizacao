6a1e0d9134c12507ec409639485c306725349e94
lunievas <luisa.nievas94@gmail.com>
2020-02-04 13:32:37 -0300

    adicionando fotos dos gatos a pagina adote_gatos

--- adote_gatos.php
+++ adote_gatos.php
@@ -141,9 +141,9 @@
                 <div class="mx-auto">
                     <div class="col-sm-4 col-xs-12" >
                         <div class="card text-center" style="width: 18rem;">
-                            <img class="card-img-top" src="images/perdido3.jpg" alt="Imagem de capa do card">
+                            <img class="card-img-top" src="images/gato1.jpg" alt="Imagem de capa do card">
                             <div class="card-body">
-                                <h5 class="card-title">Da vinci</h5>
+                                <h5 class="card-title">Chuchu</h5>
                                 <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
                                 <a href="#" class="btn btn-primary">Visitar</a>
                             </div>
@@ -154,9 +154,9 @@
                 <div class="mx-auto">
                     <div class="col-sm-4 col-xs-12" >
                         <div class="card text-center" style="width: 18rem;">
-                                <img class="card-img-top" src="images/perdido2.jpg" alt="Imagem de capa do card">
+                                <img class="card-img-top" src="images/gato02.jpg" alt="Imagem de capa do card">
                                 <div class="card-body">
-                                    <h5 class="card-title">Doido</h5>
+                                    <h5 class="card-title">Belezinha</h5>
                                     <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
                                     <a href="#" class="btn btn-primary">Visitar</a>
                                 </div>
@@ -167,9 +167,9 @@
                 <div class="mx-auto">
                     <div class="col-sm-4 col-xs-12" >    
                         <div class="card text-center" style="width: 18rem;">
-                                <img class="card-img-top" src="images/perdido1.jpg" alt="Imagem de capa do card">
+                                <img class="card-img-top" src="images/gato3.jpg" alt="Imagem de capa do card">
                                 <div class="card-body">
-                                    <h5 class="card-title">Domingo</h5>
+                                    <h5 class="card-title">Pantera</h5>
                                     <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
                                     <a href="#" class="btn btn-primary">Visitar</a>
                                 </div>
@@ -182,9 +182,9 @@
                 <div class="mx-auto">
                     <div class="col-sm-4 col-xs-12" >
                         <div class="card text-center" style="width: 18rem;">
-                            <img class="card-img-top" src="images/perdido4.jpg" alt="Imagem de capa do card">
+                            <img class="card-img-top" src="images/gato4.jpeg" alt="Imagem de capa do card">
                             <div class="card-body">
-                                <h5 class="card-title">Dahora</h5>
+                                <h5 class="card-title">Feijão</h5>
                                 <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
                                 <a href="#" class="btn btn-primary">Visitar</a>
                             </div>
@@ -195,9 +195,9 @@
                 <div class="mx-auto">
                     <div class="col-sm-4 col-xs-12" >
                         <div class="card text-center" style="width: 18rem;">
-                                <img class="card-img-top" src="images/perdido5.jpg" alt="Imagem de capa do card">
+                                <img class="card-img-top" src="images/gato5.jpeg" alt="Imagem de capa do card">
                                 <div class="card-body">
-                                    <h5 class="card-title">Gato</h5>
+                                    <h5 class="card-title">Anakin</h5>
                                     <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
                                     <a href="#" class="btn btn-primary">Visitar</a>
                                 </div>
@@ -208,9 +208,9 @@
                 <div class="mx-auto">
                     <div class="col-sm-4 col-xs-12" >    
                         <div class="card text-center" style="width: 18rem;">
-                                <img class="card-img-top" src="images/perdido6.jpg" alt="Imagem de capa do card">
+                                <img class="card-img-top" src="images/gato6.jpeg" alt="Imagem de capa do card">
                                 <div class="card-body">
-                                    <h5 class="card-title">Miauu</h5>
+                                    <h5 class="card-title">Jupiter</h5>
                                     <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
                                     <a href="#" class="btn btn-primary">Visitar</a>
                                 </div>
