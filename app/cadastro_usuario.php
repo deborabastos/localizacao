24b9c08ab4a77d258de2b90c6f379dae343d663f
lunievas <luisa.nievas94@gmail.com>
2020-04-02 17:27:56 -0300

    criando as models mensagens, cadastro_usuario, comentarios, fotos_pet, geo_localizacao, chats, cadastro_pet, alertas

---
+++ app/cadastro_usuario.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class cadastro_usuario extends Model
+{
+    //
+}
---
+++ app/geo_localizacao.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class geo_localizacao extends Model
+{
+    //
+}
