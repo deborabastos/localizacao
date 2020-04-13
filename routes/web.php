a61f3f8da37bb936294907b47d12a0099f3b6da0
deborabastos <deborabastos14@gmail.com>
2020-04-13 14:32:08 -0300

    altera routes

--- routes/web.php
+++ routes/web.php
@@ -2,28 +2,24 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', 'PetController@index');
-Route::get('/achados_caes', 'PetController@achadosCaes');
-Route::get('/achados_gatos', 'PetController@achadosGatos');
-Route::get('/achados_perfil', 'PetController@achadosPerfil'); //alterar para {id}
-Route::get('/adote_caes', 'PetController@adoteCaes');
-Route::get('/adote_gatos', 'PetController@adoteGatos');
-Route::get('/adote_perfil/{id}', 'PetController@adotePerfil');
-Route::get('/perdidos_caes', 'PetController@perdidosCaes');
-Route::get('/perdidos_gatos', 'PetController@perdidosGatos');
-Route::get('/perdidos_perfil/{id}', 'PetController@perdidosPerfil');
+
+Route::get('/achados', 'PetController@achados'); // alterar para pegar ?especie=gato ou ?especie=cao
+Route::get('/achados/create', 'PetController@achadosCreate');
+Route::get('/achados_perfil', 'PetController@achadosPerfil'); //alterar para achados/{id}
+
+Route::get('/perdidos', 'PetController@perdidos');
+Route::get('/perdidos/create', 'PetController@perdidosCreate');
+Route::get('/perdidos_perfil/{id}', 'PetController@perdidosPerfil'); //alterar para perdidos/{id}
+
+Route::get('/adote', 'PetController@adote');
+Route::get('/adote/create', 'PetController@adoteCreate');
+Route::get('/adote_perfil/{id}', 'PetController@adotePerfil'); //alterar para adote/{id}
+
 Route::get('/quem_somos', 'PetController@quemSomos');
 Route::get('/sucesso', 'PetController@sucesso');
-Route::get('/pagina_usuario', 'UserController@paginaUsuario');
+
+Route::get('/pagina_usuario', 'UserController@user');
+Route::get('/user/create', 'UserController@userCreate');
+ 
  
