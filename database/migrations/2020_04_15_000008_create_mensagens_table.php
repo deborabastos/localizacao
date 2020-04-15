1db0a3aca7c9665920de06b7eeb4a2942ddb8a4e
lunievas <luisa.nievas94@gmail.com>
2020-04-15 09:50:12 -0300

    criando as migrations atraves do plugin

---
+++ database/migrations/2020_04_15_000008_create_mensagens_table.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMensagensTable extends Migration
+{
+    /**
+     * Schema table name to migrate
+     * @var string
+     */
+    public $tableName = 'mensagens';
+
+    /**
+     * Run the migrations.
+     * @table mensagens
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create($this->tableName, function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id_mensagens');
+            $table->string('mensagem', 200);
+            $table->date('send_at');
+            $table->integer('id_usuario');
+            $table->integer('id_chats');
+
+            $table->index(["id_usuario"], 'fk_mensagens_usuario1_idx');
+
+            $table->index(["id_chats"], 'fk_mensagens_chats1_idx');
+
+
+            $table->foreign('id_chats', 'fk_mensagens_chats1_idx')
+                ->references('id_chats')->on('chats')
+                ->onDelete('no action')
+                ->onUpdate('no action');
+
+            $table->foreign('id_usuario', 'fk_mensagens_usuario1_idx')
+                ->references('id_usuario')->on('usuarios')
+                ->onDelete('no action')
+                ->onUpdate('no action');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+     public function down()
+     {
+       Schema::dropIfExists($this->tableName);
+     }
+}
