f5f213375d1c30a48fd176eb6eac3b989b39de0a
lunievas <luisa.nievas94@gmail.com>
2020-04-16 00:09:51 -0300

    criando as relações da model Usuario

--- app/Usuario.php
+++ app/Usuario.php
@@ -2,9 +2,33 @@
 
 namespace App;
 
+use App\Mensagen;
+use App\Chat;
+use App\Geo_localizacao;
+use App\Comentario;
+use App\Alerta;
 use Illuminate\Database\Eloquent\Model;
 
 class Usuario extends Model
 {
-    //
+    public function mensagem(){
+
+        return $this->hasMany(Mensagen::class);
+    }
+    public function chat(){
+
+        return $this->hasMany(Chat::class);
+    }
+    public function geo_localizacao(){
+
+        return $this->hasMany(Geo_localizacao::class);
+    }
+    public function comentario(){
+
+        return $this->hasMany(Comentario::class);
+    }
+    public function alerta(){
+
+        return $this->hasMany(Alerta::class);
+    }
 }
