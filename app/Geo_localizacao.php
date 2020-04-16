e8c0dbd4597a2e10d3e7589a98f4833b2b73d5db
lunievas <luisa.nievas94@gmail.com>
2020-04-15 23:39:51 -0300

    criando as relações da model Geo_localizacao

--- app/Geo_localizacao.php
+++ app/Geo_localizacao.php
@@ -2,9 +2,21 @@
 
 namespace App;
 
+use App\Usuario;
+use App\Alerta;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Geo_localizacao extends Model
 {
-    //
+    public function usuario(){
+
+        return $this->belongsTo(Usuario::class);
+
+    }
+
+    public function alerta(){
+
+        return $this->belongsTo(Alerta::class);
+    }
 }
