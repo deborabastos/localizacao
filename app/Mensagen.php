af08c6d6d5fcb80beb57ff6ce8270cf852465ac4
lunievas <luisa.nievas94@gmail.com>
2020-04-15 23:42:04 -0300

    criando as relações da model Mensagen

--- app/Mensagen.php
+++ app/Mensagen.php
@@ -2,9 +2,20 @@
 
 namespace App;
 
+use App\Chat;
+use App\Usuario;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Mensagen extends Model
 {
-    //
+    public function chat(){
+
+        return $this->belongsTo(Chat::class);
+    }
+
+    public function usuario(){
+
+        return $this->belongsTo(Usuario::class);
+    }
 }
