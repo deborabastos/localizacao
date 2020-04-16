47b8fb606f94d875fe98ed35bc636becf9d702c5
lunievas <luisa.nievas94@gmail.com>
2020-04-15 23:44:45 -0300

    criando as relações da model Pet

--- app/Pet.php
+++ app/Pet.php
@@ -2,9 +2,20 @@
 
 namespace App;
 
+use App\Fotos_pet;
+use App\Alerta;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Pet extends Model
 {
-    //
+    public function fotos(){
+
+        return $this->hasMany(Fotos_pet::class);
+    }
+
+    public function alerta(){
+
+        return $this->hasMany(Alerta::class);
+    }
 }
