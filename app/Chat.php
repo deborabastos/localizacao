8e3bd7e90bf58fe58c426c62868253edfc635fbb
lunievas <luisa.nievas94@gmail.com>
2020-04-15 23:33:33 -0300

    criando as relações da model Chat

--- app/Chat.php
+++ app/Chat.php
@@ -2,9 +2,20 @@
 
 namespace App;
 
+use App\Mensagen;
+use App\Usuario;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Chat extends Model
 {
-    //
+    public function mensagem(){
+
+        return $this->hasMany(Mensagen::class);
+    }
+
+    public function usuario(){
+
+        return $this->belongsTo(Usuario::class);
+    }
 }
