882bc01f5f1e2ef8056a87a86f934013db5086ae
lunievas <luisa.nievas94@gmail.com>
2020-04-15 23:28:24 -0300

    criando as relações da model Alerta

--- app/Alerta.php
+++ app/Alerta.php
@@ -2,9 +2,37 @@
 
 namespace App;
 
+
+use App\Pet;
+use App\Usuario;
+use App\Comentario;
+use App\Geo_localizacao;
 use Illuminate\Database\Eloquent\Model;
 
 class Alerta extends Model
 {
-    //
+    public function pet()
+    {
+
+        return $this->belongsTo(Pet::class);
+    }
+
+    public function usuario(){
+
+        return $this->belongsTo(Usuario::class);
+    }
+
+    public function comentario(){
+
+        return $this->hasMany(Comentario::class);
+    }
+
+    public function geo_localizacao(){
+
+        return $this->hasMany(Geo_localizacao::class);
+    }
+
+
+
+    public $timestamps = false;
 }
