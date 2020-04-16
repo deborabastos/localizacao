06b2436169290abe9351eacf951599646603aa46
lunievas <luisa.nievas94@gmail.com>
2020-04-15 23:36:15 -0300

    criando as relações da model Comentario

--- app/Comentario.php
+++ app/Comentario.php
@@ -2,9 +2,20 @@
 
 namespace App;
 
+use App\Usuario;
+use App\Alerta;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Comentario extends Model
 {
-    //
+    public function usuario(){
+
+        return $this->belongsTo(Usuario::class);
+    }
+
+    public function alerta(){
+
+        return $this->belongsTo(Alerta::class);
+    }
 }
