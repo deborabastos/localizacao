491dc914aa7ab46bae8d57fa7266a797519d3fe0
lunievas <luisa.nievas94@gmail.com>
2020-04-16 15:16:24 -0300

    criando as fillable nas models Comentario e Fotos_pet

--- app/Comentario.php
+++ app/Comentario.php
@@ -9,6 +9,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comentario extends Model
 {
+    protected $fillable = [
+        'id_comentario',
+        'comentario',
+        'created_at',
+        'id_alerta',
+        'id_usuario'
+
+
+    ];
     public function usuario(){
 
         return $this->belongsTo(Usuario::class);
--- app/Fotos_pet.php
+++ app/Fotos_pet.php
@@ -8,6 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Fotos_pet extends Model
 {
+    protected $fillable = [
+        'id_foto',
+        'link_foto',
+        'posicao_foto',
+        'id_pet',
+    ];
     public function pet(){
 
         return $this->belongsTo(Pet::class);
