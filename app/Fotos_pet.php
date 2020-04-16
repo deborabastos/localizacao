0f7235c4ceac4dbfb91e3a806f67ad1a1bc632db
lunievas <luisa.nievas94@gmail.com>
2020-04-15 23:37:50 -0300

    criando as relações da model Fotos_pet

--- app/Fotos_pet.php
+++ app/Fotos_pet.php
@@ -2,9 +2,14 @@
 
 namespace App;
 
+use App\Pet;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Fotos_pet extends Model
 {
-    //
+    public function pet(){
+
+        return $this->belongsTo(Pet::class);
+    }
 }
