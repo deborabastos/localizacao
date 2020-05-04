5692f1d47323aca6e17938e16a563f6daba62a0a
lunievas <luisa.nievas94@gmail.com>
2020-05-04 19:17:14 -0300

    criando a view de editar comentario

---
+++ resources/views/pet/editComment.blade.php
@@ -0,0 +1,62 @@
+@extends('layouts.layout')
+
+@section('title')
+LocalizaCão - Perfil
+@endsection
+
+
+@section('content')
+
+<main>
+    {{-- Mensagem de erro ou sucesso --}}
+    @include('inc.msg')
+
+    <a href="javascript:history.back()"> <<-- Voltar </a> 
+
+
+     <section class="container mt-5">
+        <div class="row m-0">
+
+        {{-- Comentários --}}
+        <div class="comentarios">
+        <form action="../editComment/{{$pet->id}}" method="POST">
+            @csrf
+                @method('PUT')
+            <div class="col-md-12">
+                {{-- <div class="border rounded"> --}}
+                    <div class="form-comment">
+                        <div class="avatar-comment">
+
+                            {{-- AVATAR DO USER --}}
+                            {{-- AVATAR DO USER --}}
+                            {{-- AVATAR DO USER --}}
+                            <img src="/images/user/2.jpeg" alt="">
+                        </div>
+
+
+                        <div class="form-group">
+                            <input type="hidden" name="id" value="{{ $pet->id }}" >
+                            {{-- <label for="exampleFormControlTextarea1"> {{$comment->user->name}} </label> --}}
+                            <textarea class="form-control mb-5" id="comment" name="comment" rows="3" >
+                                {{$comment->comment}}
+                            </textarea>
+                          </div>
+                               
+
+                            <button class="btn btn-primary btn-sm ml-5 mr-3"" type="submit">Editar </button>
+                            {{-- @csrf
+                            @method('DELETE') --}}
+                            <button class="btn btn-danger btn-sm">Excluir</button>
+                    </div>
+                </div>
+            </div>
+        </form> 
+    </div>
+
+
+
+        </div>
+    </section>
+</main>
+
+@endsection
